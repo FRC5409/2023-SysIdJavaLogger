f72555def90e2e260a9d7365c85c53004877b689
ohowe <ohowe@protonmail.com>
2022-03-05 18:40:08 -0700

    Inital commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+public class Constants {
+    private Constants() {
+
+    }
+
+    public static final int LEFT_TOP_PORT = 2;
+    public static final int LEFT_FRONT_PORT = 1;
+    public static final int LEFT_BACK_PORT = 3;
+
+    public static final int RIGHT_TOP_PORT = 8;
+    public static final int RIGHT_FRONT_PORT = 9;
+    public static final int RIGHT_BACK_PORT = 7;
+
+    public static final boolean LEFT_INVERTED = true;
+    public static final boolean RIGHT_INVERTED = false;
+
+
+    public static final double GEARING = 8.68;
+    public static final double DISTANCE_PER_ROTATION = 1 / (GEARING);
+
+    public static final int DATA_VECTOR_SIZE = 36000;
+}
